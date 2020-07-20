2543fd7317b7e6d641f3329091015c7acb813976
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-07-20 12:33:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const src = () => {
+  return (
+  
+  <View>
+    <Text>Hello</Text>
+
+  </View>
+  
+  
+  );
+}
+
+export default src;
